e9e00c99e8a7f2daa5887070ef28d0bd79fedec3
John Goiri <jg.goiri@gmail.com>
2018-07-08 01:45:00 -0700

    Ditched boost for pybind11
    So far it seems really nice

--- utilipy/casmutils/structure/structure.cxx
+++ utilipy/casmutils/structure/structure.cxx
@@ -4,85 +4,142 @@
 //  Hello World Example from the tutorial
 //  [Joel de Guzman 10/9/2002]
 
-#include <boost/python/module.hpp>
-#include <boost/python/def.hpp>
-#include <boost/python/class.hpp>
-#include <boost/python/operators.hpp>
 #include <boost/filesystem.hpp>
+/* #include <boost/python/class.hpp> */
+/* #include <boost/python/def.hpp> */
+/* #include <boost/python/list.hpp> */
+/* #include <boost/python/module.hpp> */
+/* #include <boost/python/operators.hpp> */
+#include <pybind11/pybind11.h>
 
-#include <casm/CASM_global_definitions.hh>
+#include <pybind11/stl.h>
+#include <pybind11/eigen.h>
+
 #include "casmutils/structure.hpp"
+#include <casm/CASM_global_definitions.hh>
 #include <casm/crystallography/Structure.hh>
 #include <string>
 
-char const* greet()
-{
-   return "hello, world";
-}
+#include "Eigen/Core"
 
-std::string smart_greet()
-{
-    std::string greeting="Can you do this though?";
-    return std::string();
-}
+//******************************************************************************************************//
+//******************************************************************************************************//
 
 namespace Rewrap
 {
-    class Structure : public CASM::Structure
+
+class Structure : public CASM::Structure
+{
+public:
+    Structure() = delete;
+
+    Structure(CASM::Structure init_struc);
+
+    bool is_primitive() const;
+
+    Structure primitive() const;
+
+    std::string to_string() const;
+
+private:
+};
+
+Structure from_poscar(const std::string& filename);
+void to_poscar(const Structure& writeable, const std::string& filename);
+
+//******************************************************************************************************//
+
+Structure::Structure(CASM::Structure init_struc) : CASM::Structure(init_struc) {}
+
+bool Structure::is_primitive() const { return CASM::Structure::is_primitive(); }
+
+Structure Structure::primitive() const { return Simplicity::make_primitive(*this); }
+
+std::string Structure::to_string() const
+{
+    std::ostringstream sstream;
+    Simplicity::print_poscar(*this, sstream);
+    return sstream.str();
+}
+
+Structure make_niggli(const Structure& non_niggli)
+{
+    return Simplicity::make_niggli(non_niggli);
+}
+
+Structure from_poscar(const std::string& filename) { return Structure(CASM::Structure(filename)); }
+
+void to_poscar(const Structure& writeable, const std::string& filename)
+{
+    Simplicity::write_poscar(writeable, filename);
+    return;
+}
+
+void test_vec_int(const std::vector<int>& vec)
+{
+    for(const auto& v : vec)
     {
-        public:
-
-            Structure()=delete;
-
-            Structure(CASM::Structure init_struc):CASM::Structure(init_struc){}
-
-            bool is_primitive() const
-            {
-                return CASM::Structure::is_primitive();
-            }
-
-            Structure primitive() const
-            {
-                return Simplicity::make_primitive(*this);
-            }
-
-        private:
-
-
-    };
-
-    Structure from_poscar(const std::string& filename)
-    {
-        return Structure(CASM::Structure(filename));
+        std::cout<<v<<std::endl;
     }
+}
 
-    void to_poscar(const Structure& writeable, const std::string& filename)
+void test_eigen_mat(const Eigen::Matrix3d& mat)
+{
+    for(int i=0; i<3; ++i)
     {
-        Simplicity::write_poscar(writeable, filename);
-        return;
+        for(int j=0; j<3; ++j)
+        {
+            std::cout<<mat(i,j)<<" ";
+        }
+        std::cout<<std::endl;
     }
+    return;
+}
 
-    std::ostream& operator<<(std::ostream& output, const Structure& outputtable)
-    {
-        Simplicity::print_poscar(outputtable, output);
-        return output;
-    }
+/* std::ostream& operator<<(std::ostream& output, const Structure& outputtable) */
+/* { */
+/*     Simplicity::print_poscar(outputtable, output); */
+/*     return output; */
+/* } */
 
 }
 
-BOOST_PYTHON_MODULE(_structure)
+//******************************************************************************************************//
+
+/* BOOST_PYTHON_MODULE(_structure) */
+/* { */
+/*     using namespace boost::python; */
+/*     using namespace Rewrap; */
+
+/*     class_<Structure>("Structure", no_init) */
+/*         .def("is_primitive", &Structure::is_primitive) */
+/*         .def("primitive", &Structure::primitive) */
+/*         .def(str(self)); */
+
+/*     /1* typedef std::vector<int> std_vector_int; *1/ */
+/*     /1* class_<std_vector_int>("std_vector_int").def(vector_indexing_suite<std_vector_int>()); *1/ */
+
+/*     def("from_poscar", from_poscar); */
+/*     def("to_poscar", to_poscar); */
+/*     /1* def("test_int_list", test_int_list); *1/ */
+/* } */
+
+PYBIND11_MODULE(_structure, m)
 {
-    using namespace boost::python;
+    using namespace pybind11;
     using namespace Rewrap;
 
-    class_<Structure>("Structure",no_init)
+    m.doc() = "Raw python bindings for a re-wrapped CASM::Structure class.";
+
+    class_<Structure>(m,"Structure")
         .def("is_primitive", &Structure::is_primitive)
         .def("primitive", &Structure::primitive)
-        .def(str(self))
+        .def("__repr__", &Structure::to_string)
         ;
 
-    def("from_poscar",from_poscar);
-    def("to_poscar",to_poscar);
+    m.def("from_poscar", from_poscar);
+    m.def("to_poscar", to_poscar);
+    m.def("make_niggli", make_niggli);
+    /* m.def("test_vec_int", test_vec_int); */
+    /* m.def("test_eigen_mat", test_eigen_mat); */
 }
-
-
