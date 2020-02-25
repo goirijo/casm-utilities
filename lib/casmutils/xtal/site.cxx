c18872b70a055babd5c35f32f508b78d3e8d9457
skk74@github.com <skk74@github.com>
2020-02-24 18:13:31 -0800

    clangformatted

--- lib/casmutils/xtal/site.cxx
+++ lib/casmutils/xtal/site.cxx
@@ -19,21 +19,18 @@ Site::Site(const rewrap::Coordinate& init_coord, const std::string& occupant_nam
 
 {
     // Avoid a multioccupant site
-	assert(casm_site.allowed_occupants().size()==1);
+    assert(casm_site.allowed_occupants().size() == 1);
     // Avoid an unitialized state.
-     this->casm_site.set_label(0);
+    this->casm_site.set_label(0);
 }
 
-std::string Site::label() const { 
-
-	return this->casm_site.allowed_occupants()[this->casm_site.label()]; 
-}
+std::string Site::label() const { return this->casm_site.allowed_occupants()[this->casm_site.label()]; }
 
 Eigen::Vector3d Site::cart() const { return this->casm_site.cart(); }
 
-Eigen::Vector3d Site::frac(const rewrap::Lattice& ref_lattice) const { 
-	return ref_lattice.column_vector_matrix().inverse()*this->cart();	
-
+Eigen::Vector3d Site::frac(const rewrap::Lattice& ref_lattice) const
+{
+    return ref_lattice.column_vector_matrix().inverse() * this->cart();
 }
 
 } // namespace rewrap
--- tests/unit/casmutils/site.cpp
+++ tests/unit/casmutils/site.cpp
@@ -1,9 +1,9 @@
-//these are required for test construction
+// these are required for test construction
 #include <casmutils/xtal/coordinate.hpp>
 #include <casmutils/xtal/lattice.hpp>
 #include <gtest/gtest.h>
 #include <memory>
-//tests the functions in this file
+// tests the functions in this file
 #include <casmutils/xtal/site.hpp>
 class SiteTest : public testing::Test
 {
@@ -11,15 +11,14 @@ protected:
     void SetUp() override
     {
         Eigen::Vector3d raw_coord(0.1, 0.2, 0.3);
-		rewrap::Coordinate init_position(raw_coord);
-		//store coordinate object for comparisons later
-		init_pos_ptr.reset(new rewrap::Coordinate(raw_coord));
+        rewrap::Coordinate init_position(raw_coord);
+        // store coordinate object for comparisons later
+        init_pos_ptr.reset(new rewrap::Coordinate(raw_coord));
 
-        lithium_site_ptr.reset(new rewrap::Site(init_position,"Li"));
-        nickel_site_ptr.reset(new rewrap::Site(init_position,"Ni"));
-		//store lattice for coordinate transforms
-		cubic_lattice_ptr.reset(new rewrap::Lattice(4.0*Eigen::Matrix3d::Identity()));
-
+        lithium_site_ptr.reset(new rewrap::Site(init_position, "Li"));
+        nickel_site_ptr.reset(new rewrap::Site(init_position, "Ni"));
+        // store lattice for coordinate transforms
+        cubic_lattice_ptr.reset(new rewrap::Lattice(4.0 * Eigen::Matrix3d::Identity()));
     }
 
     // Use unique pointers because Site has no default constructor
@@ -30,25 +29,27 @@ protected:
     std::unique_ptr<rewrap::Lattice> cubic_lattice_ptr;
 };
 
-
-TEST_F(SiteTest, Construct) { 
-	//This test checks construction and the const accessors
-	// to the cartesian position of the site and the 
-	// identity of the site.	
-	EXPECT_EQ(lithium_site_ptr->cart(), nickel_site_ptr->cart()); 
-	EXPECT_EQ(lithium_site_ptr->label(),"Li");
-	EXPECT_EQ(nickel_site_ptr->label(),"Ni");
+TEST_F(SiteTest, Construct)
+{
+    // This test checks construction and the const accessors
+    // to the cartesian position of the site and the
+    // identity of the site.
+    EXPECT_EQ(lithium_site_ptr->cart(), nickel_site_ptr->cart());
+    EXPECT_EQ(lithium_site_ptr->label(), "Li");
+    EXPECT_EQ(nickel_site_ptr->label(), "Ni");
 }
 
-TEST_F(SiteTest, CoordCast) {
-	//This test checks the ability to cast a Site to a Coordinate
-	EXPECT_EQ(rewrap::Coordinate(*lithium_site_ptr).cart(),init_pos_ptr->cart());
+TEST_F(SiteTest, CoordCast)
+{
+    // This test checks the ability to cast a Site to a Coordinate
+    EXPECT_EQ(rewrap::Coordinate(*lithium_site_ptr).cart(), init_pos_ptr->cart());
 };
 
-TEST_F(SiteTest, FracConversion) {
-	//This test checks the ability to get the fractional
-	// coordinates of a site with respect to a lattice
-	EXPECT_EQ(lithium_site_ptr->frac(*cubic_lattice_ptr),Eigen::Vector3d(0.025,0.05,0.075));
+TEST_F(SiteTest, FracConversion)
+{
+    // This test checks the ability to get the fractional
+    // coordinates of a site with respect to a lattice
+    EXPECT_EQ(lithium_site_ptr->frac(*cubic_lattice_ptr), Eigen::Vector3d(0.025, 0.05, 0.075));
 };
 
 int main(int argc, char** argv)
