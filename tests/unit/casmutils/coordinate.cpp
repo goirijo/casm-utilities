49991910d3050ca05d26556df99c5af1dbf0a566
seshasaibehara <seshasaibehara@gmail.com>
2020-03-06 14:17:01 -0800

    made suggested changes

--- tests/unit/casmutils/coordinate.cpp
+++ tests/unit/casmutils/coordinate.cpp
@@ -8,39 +8,30 @@ class CoordinateTest : public testing::Test{
 protected:
     void SetUp() override{
         Eigen::Vector3d raw_coord(0.1, 0.2, 0.3);
-        Eigen::Matrix3d lattice;
-        lattice << 0,0.5,0.5,
-                0.5,0,0.5,
-                0.5,0.5,0;
-        
-        coord0_ptr.reset(new rewrap::Coordinate(raw_coord));
-        coord1_ptr.reset(new rewrap::Coordinate(raw_coord(0), raw_coord(1), raw_coord(2)));
-        lattice_ptr.reset(new rewrap::Lattice(lattice));
+        lattice_matrix << 0,0.5,0.5,0.5,0,0.5,0.5,0.5,0;
+        coord0_ptr.reset(new casmutils::xtal::Coordinate(raw_coord));
+        coord1_ptr.reset(new casmutils::xtal::Coordinate(raw_coord(0), raw_coord(1), raw_coord(2)));
+        lattice_ptr.reset(new casmutils::xtal::Lattice(lattice_matrix));
+        frac_coords = lattice_matrix.inverse()*coord0_ptr->cart(); 
     }
     // Use unique pointers because Coordinate has no default constructor
-    std::unique_ptr<rewrap::Coordinate> coord0_ptr;
-    std::unique_ptr<rewrap::Coordinate> coord1_ptr;
-    std::unique_ptr<rewrap::Lattice> lattice_ptr;
+    std::unique_ptr<casmutils::xtal::Coordinate> coord0_ptr;
+    std::unique_ptr<casmutils::xtal::Coordinate> coord1_ptr;
+    std::unique_ptr<casmutils::xtal::Lattice> lattice_ptr;
+    Eigen::Matrix3d lattice_matrix;
+    Eigen::Vector3d frac_coords;
 };
 
 TEST_F(CoordinateTest, Construct) { EXPECT_EQ(coord0_ptr->cart(), coord1_ptr->cart()); }
 
-TEST_F(CoordinateTest, FracRetrieve){
-    rewrap::Lattice &lattice_rewrap = *lattice_ptr;
-    Eigen::Matrix3d lattice;
-    lattice << 0,0.5,0.5,0.5,0,0.5,0.5,0.5,0;
-    Eigen::Vector3d frac_coords = lattice.inverse()*coord0_ptr->cart();
-  
-    rewrap::Coordinate coords_rewrap = rewrap::Coordinate::from_fractional(frac_coords,lattice_rewrap);     
-    EXPECT_TRUE(frac_coords.isApprox(coords_rewrap.frac(lattice_rewrap)));
+TEST_F(CoordinateTest, FracRetrieve){ 
+    casmutils::xtal::Coordinate coords_rewrap = casmutils::xtal::Coordinate::from_fractional(frac_coords,*lattice_ptr);     
+    EXPECT_TRUE(frac_coords.isApprox(coords_rewrap.frac(*lattice_ptr)));
 }
 
 TEST_F(CoordinateTest, BringWithIn){
-    Eigen::Matrix3d lattice;
-    lattice << 0,0.5,0.5,0.5,0,0.5,0.5,0.5,0;
-    Eigen::Vector3d frac_coords = lattice.inverse()*coord0_ptr->cart();
     //Bring within using the member function
-    rewrap::Coordinate coords_rewrap = rewrap::Coordinate::from_fractional(frac_coords,*lattice_ptr);
+    casmutils::xtal::Coordinate coords_rewrap = casmutils::xtal::Coordinate::from_fractional(frac_coords,*lattice_ptr);
     coords_rewrap.bring_within(*lattice_ptr);  
     //Bring within using formuation
     for (int i=0; i<3; ++i){
