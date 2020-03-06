5e1ba9d7e99aebd0107a4974a93bd6d9d59de8ed
seshasaibehara <seshasaibehara@gmail.com>
2020-03-06 14:41:56 -0800

    made suggested changes

--- tests/unit/casmutils/coordinate.cpp
+++ tests/unit/casmutils/coordinate.cpp
@@ -6,33 +6,37 @@
 
 class CoordinateTest : public testing::Test{
 protected:
-    void SetUp() override{
-        Eigen::Vector3d raw_coord(0.1, 0.2, 0.3);
-        lattice_matrix << 0,0.5,0.5,0.5,0,0.5,0.5,0.5,0;
-        coord0_ptr.reset(new casmutils::xtal::Coordinate(raw_coord));
-        coord1_ptr.reset(new casmutils::xtal::Coordinate(raw_coord(0), raw_coord(1), raw_coord(2)));
-        lattice_ptr.reset(new casmutils::xtal::Lattice(lattice_matrix));
-        frac_coords = lattice_matrix.inverse()*coord0_ptr->cart(); 
-    }
+    using Coordinate=casmutils::xtal::Coordinate;
+    using Lattice=casmutils::xtal::Lattice;
     // Use unique pointers because Coordinate has no default constructor
-    std::unique_ptr<casmutils::xtal::Coordinate> coord0_ptr;
-    std::unique_ptr<casmutils::xtal::Coordinate> coord1_ptr;
-    std::unique_ptr<casmutils::xtal::Lattice> lattice_ptr;
+    std::unique_ptr<Coordinate> coord0_ptr;
+    std::unique_ptr<Coordinate> coord1_ptr;
+    std::unique_ptr<Lattice> lattice_ptr;
+    std::unique_ptr<Coordinate> coord2_ptr;
     Eigen::Matrix3d lattice_matrix;
     Eigen::Vector3d frac_coords;
+
+    void SetUp() override{
+
+        Eigen::Vector3d raw_coord(0.1, 0.2, 0.3);
+        lattice_matrix << 0,0.5,0.5,0.5,0,0.5,0.5,0.5,0;
+        coord0_ptr.reset(new Coordinate(raw_coord));
+        coord1_ptr.reset(new Coordinate(raw_coord(0), raw_coord(1), raw_coord(2)));
+        lattice_ptr.reset(new Lattice(lattice_matrix));
+        frac_coords = lattice_matrix.inverse()*coord0_ptr->cart(); 
+        coord2_ptr.reset(new Coordinate(Coordinate::from_fractional(frac_coords,*lattice_ptr)));
+    }
 };
 
 TEST_F(CoordinateTest, Construct) { EXPECT_EQ(coord0_ptr->cart(), coord1_ptr->cart()); }
 
 TEST_F(CoordinateTest, FracRetrieve){ 
-    casmutils::xtal::Coordinate coord2 = casmutils::xtal::Coordinate::from_fractional(frac_coords,*lattice_ptr);
-    EXPECT_TRUE(frac_coords.isApprox(coord2.frac(*lattice_ptr)));
+    EXPECT_TRUE(frac_coords.isApprox(coord2_ptr->frac(*lattice_ptr)));
 }
 
 TEST_F(CoordinateTest, BringWithIn){
     //Bring within using the member function
-    casmutils::xtal::Coordinate coord2 = casmutils::xtal::Coordinate::from_fractional(frac_coords,*lattice_ptr);
-    coord2.bring_within(*lattice_ptr);  
+    coord2_ptr->bring_within(*lattice_ptr);  
     //Bring within using formuation
     for (int i=0; i<3; ++i){
         if (frac_coords(i)>=1 || frac_coords(i)<0){
@@ -40,7 +44,7 @@ TEST_F(CoordinateTest, BringWithIn){
         }
     }
     
-    EXPECT_TRUE(coord2.frac(*lattice_ptr).isApprox(frac_coords));
+    EXPECT_TRUE(coord2_ptr->frac(*lattice_ptr).isApprox(frac_coords));
 }
 TEST_F(CoordinateTest, PlusEqualOperator){
     Eigen::Vector3d coord_to_compare(0.2,0.4,0.6);
