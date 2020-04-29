3f2062af0372541c076f30b2c0b770b1f7070e75
skk74@github.com <skk74@github.com>
2020-04-29 11:59:09 -0700

    added symmetrize for structure and C++ tests

--- include/casmutils/xtal/symmetry.hpp
+++ include/casmutils/xtal/symmetry.hpp
@@ -20,18 +20,16 @@ std::vector<sym::CartOp> make_point_group(const Lattice& lat, double tol);
 /// crystal structure onto itself.
 std::vector<sym::CartOp> make_factor_group(const Structure& struc, double tol);
 
-// TODO: Commented out until someone shows that this actually works
 /// Modify the given Lattice such that it perfectly obeys the provided
 /// symmetry group. Useful for reducing noise in lattice vectors.
 Lattice symmetrize(const Lattice& noisy_lattice, const std::vector<sym::CartOp>& enforced_point_group);
 
-// TODO: Does this even make sense?
 /// Modify the given Structure such that it perfectly obeys the provided
 /// symmetry group. Useful for reducing noise in lattice vectors and basis.
 /// First, the group will by applied to the lattice, to even out the vectors.
 /// The same operations will then be applied to the basis and the resulting values
 /// will be averaged out.
-/* Lattice symmetrize(const Lattice& noisy_lattice, const std::vector<sym::CartOp>& enforced_point_group); */
+Structure symmetrize(const Structure& noisy_structure, const std::vector<sym::CartOp>& enforced_factor_group);
 
 } // namespace xtal
 } // namespace casmutils
--- tests/unit/casmutils/xtal/symmetry.cpp
+++ tests/unit/casmutils/xtal/symmetry.cpp
@@ -28,6 +28,7 @@ class SymmetrizeTest : public testing::Test
 {
 protected:
     using Lattice = cu::xtal::Lattice;
+    using Structure = cu::xtal::Structure;
 
     void SetUp() override
     {
@@ -36,11 +37,21 @@ protected:
         tetragonal_lat_matrix(2, 2) = 4;
         cubic_lat_ptr = std::make_unique<Lattice>(cubic_lat_matrix);
         tetragonal_lat_ptr = std::make_unique<Lattice>(tetragonal_lat_matrix);
+
+
+        cu::fs::path hcp_path(cu::autotools::input_filesdir / "Mg_hcp.vasp");
+        hcp_Mg_ptr = std::make_unique<Structure>(Structure::from_poscar(hcp_path));
+
+        cu::fs::path almost_hcp_path(cu::autotools::input_filesdir / "distorted_Mg_hcp.vasp");
+        almost_hcp_Mg_ptr = std::make_unique<Structure>(Structure::from_poscar(almost_hcp_path));
+
     }
 
     std::unique_ptr<Lattice> cubic_lat_ptr;
     std::unique_ptr<Lattice> tetragonal_lat_ptr;
-    double tol = 1e-10;
+    std::unique_ptr<Structure> hcp_Mg_ptr;
+    std::unique_ptr<Structure> almost_hcp_Mg_ptr;
+    double tol = 1e-5;
 };
 
 TEST_F(CrystalGroupTest, PointGroupSize)
@@ -62,6 +73,14 @@ TEST_F(SymmetrizeTest, LatticeSymmetrize)
     std::vector<cu::sym::CartOp> symmetrized_tetragonal_point_group =
         cu::xtal::make_point_group(symmetrized_tetragonal_lattice, tol);
     EXPECT_EQ(cubic_point_group.size(), symmetrized_tetragonal_point_group.size());
+}
+TEST_F(SymmetrizeTest, StructureSymmetrize)
+{
+	std::vector<cu::sym::CartOp> hcp_factor_group = cu::xtal::make_factor_group(*hcp_Mg_ptr,tol);
+	EXPECT_NE(cu::xtal::make_factor_group(*almost_hcp_Mg_ptr,tol).size(),hcp_factor_group.size());
+	cu::xtal::Structure symmetrized_structure = cu::xtal::symmetrize(*almost_hcp_Mg_ptr,hcp_factor_group);
+	std::vector<cu::sym::CartOp> sym_distorted_factor_group = cu::xtal::make_factor_group(symmetrized_structure,tol);
+	EXPECT_EQ(sym_distorted_factor_group.size(),hcp_factor_group.size());
 }
 int main(int argc, char** argv)
 {
