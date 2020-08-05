90b612b0a0c72ba825af5513ba2fa7e8cbe4c3b8
John Goiri <jg.goiri@gmail.com>
2020-08-04 19:32:40 -0700

    Imported shift routines from multishift

---
+++ include/casmutils/mush/shift.hpp
@@ -0,0 +1,80 @@
+#ifndef MUSH_SHIFT_HH
+#define MUSH_SHIFT_HH
+
+#include "casmutils/xtal/lattice.hpp"
+#include <casmutils/xtal/structure.hpp>
+#include <utility>
+#include <vector>
+
+namespace casmutils
+{
+namespace mush
+{
+/// Simple data structure to keep track of what index corresponds to a particular shift.
+/// When generating a uniform grid of shifts, only a density along each a anb b direction
+/// needs to be specified, and then all shifts are retuned in an unrolled vector.
+/// This data container keeps track of the index along the a direction, b direction, and
+/// position of the unrolled vector.
+struct ShiftRecord
+{
+    ShiftRecord(int a, int b, int index) : a(a), b(b), index(index) {}
+
+    int index;
+    int a;
+    int b;
+
+    bool operator==(const ShiftRecord& lhs) const
+    {
+        return (this->a==lhs.a && this->b==lhs.b && this->index==lhs.index);
+    }
+};
+
+inline std::ostream& operator<<(std::ostream& os, const ShiftRecord& sr)
+{
+    os<<sr.a<<", "<<sr.b<<": "<<sr.index;
+    return os;
+}
+
+/// Adds the specified vector to the c-vector of the lattice. Can be used to create shifted
+/// structures (in plane vector) or cleaved structures (perpendicular to plane vector)
+xtal::Lattice mutate(const xtal::Lattice& lat, const Eigen::Vector3d& c_vector_mutation);
+xtal::Structure mutate(const xtal::Structure& struc, const Eigen::Vector3d& c_vector_mutation);
+
+/// For each cleavage value, create a new structure that has that amount of empty space over the a-b plane
+std::vector<xtal::Structure> make_cleaved_structures(const xtal::Structure& slab, const std::vector<double>& cleavage_values);
+
+/// Given a density along each of the a and b directions, create an unrolled vector of shifts
+/// along the ab-plane with a uniform distribution. The density specifies the number of unique
+/// shifts, so periodic images are NOT counted in the density. This means that even numbers yield
+/// shifts at midway points. For example, a 2x2 grid gives 4 points: origin, halfway along a,
+/// halfway along b, and dead center of the ab-plane.
+/// Two values are returned: a vector of the shifts themselves, and a vector of ShiftRecord, one
+/// for each shift, which relate integer representations of the shifts to the correct index
+/// of the vector of shifts. Following the previous example, the shifts and their corresponding
+/// records might be:
+///
+/// origin : {0,0,0}
+/// halfway along b: {0,1,1}
+/// halfway along a: {1,0,2}
+/// center of plane: {1,1,3}
+std::pair<std::vector<Eigen::Vector3d>, std::vector<ShiftRecord>>
+make_uniform_in_plane_shift_vectors(const xtal::Lattice& slab_lattice, int a_max, int b_max);
+
+/// Identical to make_uniform_in_plane_shift_vectors, except the sifts are kept within the
+/// Wigner-Seitz cell.
+std::pair<std::vector<Eigen::Vector3d>, std::vector<ShiftRecord>>
+make_uniform_in_plane_wigner_seitz_shift_vectors(const xtal::Lattice& slab_lattice, int a_max, int b_max);
+
+/// Given a list of shift vectors parallel to the ab-plane of the structure, make a new structure
+/// such that the periodic images along the vertical direction have been shifted by that amount.
+/// The resulting structre will be a series of staggered slabs
+std::vector<xtal::Structure> make_shifted_structures(const xtal::Structure& slab, std::vector<Eigen::Vector3d>& shifts);
+
+/// Given a list of slab structures with different shifts applied, return a list of indexes
+/// that describe which structures are equivalent to each other. The vector at index i contains
+/// all the indexes of the structures that are equivalent to the structure at index i.
+std::vector<std::vector<std::size_t>> categorize_equivalently_shifted_structures(const std::vector<xtal::Structure>& shifted_structures);
+} // namespace mush
+}
+
+#endif
---
+++ lib/casmutils/mush/shift.cxx
@@ -0,0 +1,127 @@
+#include <casmutils/definitions.hpp>
+#include <casmutils/mapping/structure_mapping.hpp>
+#include <casmutils/mush/shift.hpp>
+#include <casmutils/xtal/coordinate.hpp>
+#include <casmutils/xtal/lattice.hpp>
+#include <casmutils/xtal/structure.hpp>
+#include <cassert>
+#include <utility>
+
+namespace casmutils
+{
+namespace mush
+{
+xtal::Lattice mutate(const xtal::Lattice& lat, const Eigen::Vector3d& c_vector_mutation)
+{
+    return xtal::Lattice(lat.a(), lat.b(), lat.c() + c_vector_mutation);
+}
+
+xtal::Structure mutate(const xtal::Structure& struc, const Eigen::Vector3d& c_vector_mutation)
+{
+    auto mutated_lattice = mutate(struc.lattice(), c_vector_mutation);
+    return struc.set_lattice(mutated_lattice, xtal::CART);
+}
+
+std::vector<xtal::Structure> make_cleaved_structures(const xtal::Structure& slab,
+                                                         const std::vector<double>& cleavage_values)
+{
+    // TODO: This could go astray if you get a left handed lattice <-- pls explain? I don't see it
+    std::vector<xtal::Structure> cleaved_structures;
+
+    Eigen::Vector3d unit_normal = slab.lattice().a().cross(slab.lattice().b()).normalized();
+    for (double cleave : cleavage_values)
+    {
+        /* Eigen::Vector3d new_c_vector = slab.lattice().c() + cleave * unit_normal; */
+        /* xtal::Lattice cleaved_lattice(slab.lattice().a(), slab.lattice().b(), new_c_vector); */
+
+        /* cleaved_structures.emplace_back(slab.set_lattice(cleaved_lattice, xtal::CART)); */
+        cleaved_structures.emplace_back(mutate(slab, cleave * unit_normal));
+    }
+    return cleaved_structures;
+}
+
+std::pair<std::vector<Eigen::Vector3d>, std::vector<ShiftRecord>>
+make_uniform_in_plane_shift_vectors(const xtal::Lattice& slab_lattice, int a_max, int b_max)
+{
+    std::vector<Eigen::Vector3d> shifts;
+    std::vector<ShiftRecord> records;
+    int ix = 0;
+    for (int a = 0; a < a_max; ++a)
+    {
+        for (int b = 0; b < b_max; ++b)
+        {
+            Eigen::Vector3d frac_coord(static_cast<double>(a) / a_max, static_cast<double>(b) / b_max, 0);
+            shifts.emplace_back(xtal::make_fractional(frac_coord, slab_lattice));
+
+            records.emplace_back(a, b, ix);
+            ++ix;
+        }
+    }
+    return std::make_pair(std::move(shifts), std::move(records));
+}
+
+std::pair<std::vector<Eigen::Vector3d>, std::vector<ShiftRecord>>
+make_uniform_in_plane_wigner_seitz_shift_vectors(const xtal::Lattice& slab_lattice, int a_max, int b_max)
+{
+    auto standard_shifts_and_records = make_uniform_in_plane_shift_vectors(slab_lattice, a_max, b_max);
+    const auto& standard_shifts = standard_shifts_and_records.first;
+
+    std::vector<Eigen::Vector3d> wigner_seitz_shifts;
+    for (const auto& standard_shift : standard_shifts)
+    {
+        wigner_seitz_shifts.emplace_back(xtal::bring_within_wigner_seitz(standard_shift, slab_lattice));
+    }
+
+    return std::make_pair(wigner_seitz_shifts, std::move(standard_shifts_and_records.second));
+}
+
+std::vector<xtal::Structure> make_shifted_structures(const xtal::Structure& slab,
+                                                         std::vector<Eigen::Vector3d>& shifts)
+{
+    // for debugging:
+    Eigen::Vector3d plane_normal = slab.lattice().a().cross(slab.lattice().b());
+
+    std::vector<xtal::Structure> shifted_structures;
+    for (const Eigen::Vector3d& shift : shifts)
+    {
+        assert(std::abs(shift.dot(plane_normal)) < 1e-10);
+
+        /* xtal::Lattice shifted_lat(slab.lattice().a(),slab.lattice().b(),slab.lattice().c()+shift); */
+        /* shifted_structures.emplace_back(slab.set_lattice(shifted_lat,xtal::CART)); */
+        shifted_structures.emplace_back(mutate(slab, shift));
+    }
+    return shifted_structures;
+}
+
+mapping::MappingInput make_shifted_structures_categorization_map_strategy()
+{
+    mapping::MappingInput map_strategy;
+    map_strategy.k_best_maps = 0;
+    map_strategy.min_cost = 1e-8;
+    map_strategy.use_crystal_symmetry = true;
+
+    return map_strategy;
+}
+
+std::vector<std::vector<std::size_t>>
+categorize_equivalently_shifted_structures(const std::vector<xtal::Structure>& shifted_structures)
+{
+    std::vector<std::vector<std::size_t>> index_map;
+    mapping::MappingInput map_strategy = make_shifted_structures_categorization_map_strategy();
+    for (const auto shifted_structure : shifted_structures)
+    {
+        mapping::StructureMapper_f map_to_shifted(shifted_structure, map_strategy);
+        index_map.push_back({});
+        for (int s_ix = 0; s_ix < shifted_structures.size(); ++s_ix)
+        {
+            auto results = map_to_shifted(shifted_structures[s_ix]);
+            if (results.size() > 0)
+            {
+                index_map.back().emplace_back(s_ix);
+            }
+        }
+    }
+    return index_map;
+}
+} // namespace mush
+} // namespace casmutils
