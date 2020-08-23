274fcf59ad94a9bcb84d404266d337405f8f3034
John Goiri <jg.goiri@gmail.com>
2020-08-23 13:47:55 -0700

    c++ tests for mush pass

--- include/casmutils/definitions.hpp
+++ include/casmutils/definitions.hpp
@@ -1,6 +1,8 @@
 #ifndef CASM_UTILS_DEFINITIONS
 #define CASM_UTILS_DEFINITIONS
 
+#include <casm/misc/CASM_math.hh>
+#include <casm/misc/CASM_Eigen_math.hh>
 #include "casm/global/definitions.hh"
 #include "casm/global/enum.hh"
 #include <filesystem>
@@ -8,6 +10,11 @@
 namespace casmutils
 {
 namespace fs = std::filesystem;
+/* using nlohmann::json; */
+using CASM::almost_equal;
+using CASM::iround;
+using CASM::lround;
+using CASM::round;
 } // namespace casmutils
 
 namespace utilities
