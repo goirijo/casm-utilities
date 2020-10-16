d98f481ad00c6aa1782effb5f973bc519bd25277
John Goiri <jg.goiri@gmail.com>
2020-10-15 21:45:03 -0700

    Allows plugin compilation, but no documentation yet

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,14 @@
+include_sockets()
+{
+    target=plugins/Makesocket.am
+    echo "" > ${target}
+    for f in $(find plugins -mindepth 2 -maxdepth 2 -name Makesocket.am); do
+        echo "include ${f}" >> ${target}
+    done
+}
+
+echo "Appending plugins to Makefiles"
+include_sockets
+
 autoreconf -ifv
+
